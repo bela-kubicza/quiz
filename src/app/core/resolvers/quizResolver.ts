3e2855a9495de35c64381d8229597a7c1ebe60a3
Bela Kubicza <bela.kubicza@gmail.com>
2024-02-08 22:34:16 +0000

    Simplify resolver

--- src/app/core/resolvers/quizResolver.ts
+++ src/app/core/resolvers/quizResolver.ts
@@ -1,12 +1,8 @@
-import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { ResolveFn } from '@angular/router';
 import { ApiService } from '../services/api.service';
 import { inject } from '@angular/core';
 import { Observable, take } from 'rxjs';
 import { Quiz } from '../models/quiz';
 
 export const quizResolver: ResolveFn<Observable<Quiz[]>> =
-  (): Observable<Quiz[]> => {
-    const apiService: ApiService = inject(ApiService);
-
-    return apiService.getQuizzes();
-  };
+  (): Observable<Quiz[]> => inject(ApiService).getQuizzes();
