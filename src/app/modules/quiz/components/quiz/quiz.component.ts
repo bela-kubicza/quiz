c2b23a003556c04a396508f9f188e912ac5f9f53
Bela Kubicza <bela.kubicza@gmail.com>
2024-02-11 00:09:26 +0000

    Add quiz result state and restart function

--- src/app/modules/quiz/components/quiz/quiz.component.ts
+++ src/app/modules/quiz/components/quiz/quiz.component.ts
@@ -12,13 +12,13 @@ export class QuizComponent {
   @Input('quizzes') quizzes: Quiz[] = [];
 
   public readonly State = State;
+
   public state: State = State.QUIZ_TOPIC_SELECTOR;
   public selectedQuizIndex: number = NOT_SELECTED;
   public currentQuestionIndex: number = NOT_SELECTED;
   public rightOptionIndex: number = NOT_SELECTED;
   public selectedOptionIndex: number = NOT_SELECTED;
-
-  private rightAnswerCount: number = 0;
+  public rightAnswerCount: number = 0;
 
   public onQuizTopicSelect(index: number): void {
     this.selectedQuizIndex = index;
@@ -31,21 +31,34 @@ export class QuizComponent {
   }
 
   public onAnswerSubmit(): void {
-    this.rightOptionIndex = this.getRightOption();
+    this.rightOptionIndex = this.getRightOptionIndex();
 
     if (this.rightOptionIndex === this.selectedOptionIndex) {
       this.rightAnswerCount += 1;
     }
 
-    setTimeout(()=> {
-      this.rightOptionIndex = NOT_SELECTED;
-      this.currentQuestionIndex += 1;
-    }, 2000);
+    if(this.currentQuestionIndex < this.quizzes[this.selectedQuizIndex].questions.length - 1) {
+      setTimeout(()=> {
+        this.rightOptionIndex = NOT_SELECTED;
+        this.currentQuestionIndex += 1;
+      }, 2000);
+    } else {
+      this.state = State.QUIZ_RESULT
+    }
   }
 
-  public getRightOption(): number {
+  public getRightOptionIndex(): number {
     const currentQuestion: QuizQuestion = this.quizzes[this.selectedQuizIndex].questions[this.currentQuestionIndex];
 
     return currentQuestion.options.indexOf(currentQuestion.answer);
+  }
+
+  public reset() {
+    this.state = State.QUIZ_TOPIC_SELECTOR;
+    this.selectedQuizIndex = NOT_SELECTED;
+    this.currentQuestionIndex = NOT_SELECTED;
+    this.rightOptionIndex = NOT_SELECTED;
+    this.selectedOptionIndex = NOT_SELECTED;
+    this.rightAnswerCount = 0;
   }
 }
